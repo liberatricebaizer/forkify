0639a8e69c2ed43724d8e3b62ff538b014a9a0e7
Liberatrice Baizer <LiberatriceBaizer@gmail.com>
2022-08-23 07:44:30 +0200

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '43f98da9-11af-43d7-8f57-4372969afc45';
+export const MODAL_CLOSE_SEC = 2.5;
